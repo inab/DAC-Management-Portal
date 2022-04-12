fc8bf4272df68069a956ee12d138bf195aa3708f
acavalls <alejandro.canosa@bsc.es>
2022-04-12 09:40:15 +0200

    XML parser: Extract filesId instead of hrefs

--- src/lib/parseXml.js
+++ src/lib/parseXml.js
@@ -5,7 +5,7 @@ const parseXml = async (xml) => {
         xml2js.parseString(xml, (error, result) => {
             if (error) reject(error);
             let jsonString = JSON.stringify(result, null, 4)
-            let parsed = JSON.parse(jsonString)["d:multistatus"]["d:response"].map(el => el["d:href"])
+            let parsed = JSON.parse(jsonString)["d:multistatus"]["d:response"].map(el => el["d:propstat"][0]["d:prop"])
             resolve(parsed);
         });
     })
