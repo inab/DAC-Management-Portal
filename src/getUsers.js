cc55c50d0ea84624fd528c8dc5f59a8b2db078b1
acavalls <alejandro.canosa@bsc.es>
2022-04-12 09:41:04 +0200

    Keycloak client: Get single user data

--- src/getUsers.js
+++ src/getUsers.js
@@ -1,7 +1,7 @@
 import KcAdminClient from 'keycloak-admin';
 import { keycloakAdminCredentials } from './config'
 
-export default async () => {
+const initKcClient = async () => {
     const kcAdminClient = new KcAdminClient({
         baseUrl: process.env.KEYCLOAK_URL,
         realmName: 'master'
@@ -13,5 +13,18 @@ export default async () => {
         realmName: 'IPC',
     });
 
-    return await kcAdminClient.users.find()
+    return await kcAdminClient
+
 }
+
+const getUsers = async () => {
+    const kcClient = await initKcClient();
+    return await kcClient.users.find()
+}
+
+const getUserByUsername = async (id) => {
+    const kcClient = await initKcClient();
+    return await kcClient.users.findOne({username: id})
+}
+
+export { getUsers, getUserByUsername }
