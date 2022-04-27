b2678665c263173f9acb3dd3ccfd32503c120a06
acavalls <alejandro.canosa@bsc.es>
2022-04-27 14:02:46 +0200

    Panel (pages): Updating SCSS card classes

--- pages/panel/index.js
+++ pages/panel/index.js
@@ -34,25 +34,25 @@ export default function Panel() {
                 </h2>
                 <div class="panel">
                     <Link href="/panel/review">
-                        <a class="card">
+                        <a class="card-panel">
                             <h2> Review DACs </h2>
                             <p> Validate DAC info supplied by the DAC-admins.  </p>
                         </a>
                     </Link>
                     <Link href="/panel/create">
-                        <a class="card">
+                        <a class="card-panel">
                             <h2> Create a DAC </h2>
                             <p> Link users with resources and generate new DACs. </p>
                         </a>
                     </Link>
                     <Link href="/panel/roles">
-                        <a class="card">
+                        <a class="card-panel">
                             <h2> Manage DAC roles </h2>
                             <p> Manage roles from existing DACs. </p>
                         </a>
                     </Link>
                     <Link href="/panel/resources">
-                        <a class="card">
+                        <a class="card-panel">
                             <h2> Manage Resources </h2>
                             <p> Manage resources allocated to the DACs.  </p>
                         </a>
