2b732a4af263920dbde4d6519bbb2b5d7a12bd40
acavalls <alejandro.canosa@bsc.es>
2022-05-02 10:58:35 +0200

    MongoDB: Updating connection URI string (+replica set)

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,11 @@
 import { MongoClient } from 'mongodb'
 
-const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.DAC_PORTAL_DB}?authSource=${process.env.MONGO_AUTH}`;
+const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.DAC_PORTAL_DB}?connectTimeoutMS=300000&replicaSet=${process.env.MONGO_REPLICASET}&authSource=${process.env.MONGO_AUTH}`;
 const options = {}
 
 let client;
 let clientPromise;
-client = new MongoClient(uri, options)
+client = new MongoClient(uri)
 clientPromise = client.connect()
 
 export default clientPromise
