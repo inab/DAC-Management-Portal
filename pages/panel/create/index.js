f84eb009e8df70a7c8d5cb7529c892f2b41f5125
acavalls <alejandro.canosa@bsc.es>
2022-06-02 13:22:52 +0200

    Create DACs (Page): Removing hardcoded groups and instead getting DAC manager accessible folders (submissions) by requesting Nextcloud via WebDAV

--- pages/panel/create/index.js
+++ pages/panel/create/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
+import { basicAuthRequest } from '../../../src/lib/requests';
+import { parseGroupFoldersXml } from '../../../src/lib/parseXml';
 
 export default function create(data) {
   return (
@@ -29,7 +31,28 @@ export default function create(data) {
 }
 
 export async function getServerSideProps() {
-  let groups = ["IGTP", "R2", "CURIE", "DREAM", "DEMONSTRATOR"]
+
+  let filesRequest = {
+    method: 'PROPFIND',
+    endpoint: `/remote.php/dav/files/${process.env.NEXTCLOUD_DAC_MANAGER_USER}`,
+    data: `<?xml version="1.0" encoding="UTF-8"?>
+    <d:propfind xmlns:d="DAV:">
+      <d:prop xmlns:oc="http://owncloud.org/ns">
+        <d:getlastmodified/>
+        <d:getcontentlength/>
+        <d:getcontenttype/>
+        <oc:permissions/>
+        <d:resourcetype/>
+        <d:getetag/>
+      </d:prop>
+    </d:propfind>`
+  }
+
+  const xmlFiles = await basicAuthRequest(filesRequest);
+
+  const parsedXml = await parseGroupFoldersXml(xmlFiles.data);
+
+  const groups = parsedXml.folders.map(el => el.split("/").slice(-2).reverse().pop()).slice(1)
 
   return {
     props: {
