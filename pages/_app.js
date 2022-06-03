2f31a5c6409ffe347a4d13f14ef4e37727035148
acavalls <alejandro.canosa@bsc.es>
2022-06-03 14:43:51 +0200

    Adding Navbar as a Higher-Order-Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/sass/App.scss';
+import { useRouter } from "next/router";
+import Navbar from '../src/components/Navbar';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const showNavbar = router.pathname === "/" ? false : true;
+
   return (
     <div>
+      {showNavbar && <Navbar/>}
       <Component {...pageProps} />
     </div>
   )
