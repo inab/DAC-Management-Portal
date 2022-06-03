356c7917c9ce6fde3348ec508c20dad701f02614
acavalls <alejandro.canosa@bsc.es>
2022-06-03 14:43:08 +0200

    Component: Routes - Navbar & pages routing

---
+++ src/components/Routes.js
@@ -0,0 +1,9 @@
+const navlinks = [
+    { text: "Panel", href: "/panel" },
+    { text: "Review", href: "/panel/review" },
+    { text: "Create", href: "/panel/create" },
+    { text: "Roles", href: "/panel/roles" },
+    { text: "Resources", href: "/panel/resources" }
+];
+
+export default navlinks;
