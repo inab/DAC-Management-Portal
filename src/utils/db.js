785736658a27171ff6951226b1bfaab2aa822805
acavalls <alejandro.canosa@bsc.es>
2022-07-05 19:21:03 +0200

    Deployment setup: Configuring Dockerfiles, npm scripts, env, MongoDB for working on different environments

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,14 @@
 import { MongoClient } from 'mongodb'
 
-const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.DAC_PORTAL_DB}?connectTimeoutMS=300000&replicaSet=${process.env.MONGO_REPLICASET}&authSource=${process.env.MONGO_AUTH}`;
-const options = {}
+let host = process.env.NEXT_PUBLIC_APP_ENV === 'prod' ? process.env.MONGO_HOST : process.env.MONGO_HOST_TEST;	
+let db = process.env.MONGO_DB_MANAGEMENT;
+let username = process.env.MONGO_USER;
+let password = process.env.MONGO_PASS;
+let authSource = process.env.MONGO_AUTH;
+let replicaSet = process.env.MONGO_REPLICASET;
+
+let uri = process.env.NEXT_PUBLIC_APP_ENV === 'prod' ? `mongodb://${username}:${password}@${host}/${db}?authSource=${authSource}` :
+        `mongodb://${username}:${password}@${host}/${db}?connectTimeoutMS=300000&replicaSet=${replicaSet}&authSource=${authSource}`
 
 let client;
 let clientPromise;
